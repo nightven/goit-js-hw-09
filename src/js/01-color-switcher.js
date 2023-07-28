c25f98fcfdd56a571408514af211f88768121434
Vitaliy Beyar <37002507+nightven@users.noreply.github.com>
2023-07-28 12:00:11 +0200

    added first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const body = document.querySelector('body');
+const isStart = false;
+let intervalId;
 
+
+body.addEventListener('click', onClick);
+
+function onClick(e) {
+  if (e.target.hasAttribute('data-start')) {
+    e.target.setAttribute('disabled', '');
+    intervalId = setInterval(setColorToBody, 1000);
+  }
+  if (e.target.hasAttribute('data-stop')) { 
+      body.children[1].removeAttribute('disabled')
+    clearInterval(intervalId);
+  }
+}
+
+function setColorToBody() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
