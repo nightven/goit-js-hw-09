7f5c2578d8a170de341c297024b3fa6c682349ea
Vitaliy Beyar <37002507+nightven@users.noreply.github.com>
2023-07-30 09:44:04 +0200

    added 3 complited task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  let delay = Number(e.target.delay.value);
+  const step = Number(e.target.step.value);
+  const amount = Number(e.target.amount.value);
+  let position = 1;
+
+    const intervalId = setInterval(() => {
+      
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+      position++;
+
+    if (position > amount) {
+      clearInterval(intervalId);
+    } else {
+      delay += step;
+    }
+  }, delay);
+  
+}
+
 function createPromise(position, delay) {
+
   const shouldResolve = Math.random() > 0.3;
+  const promise = { position, delay };
+
   if (shouldResolve) {
-    // Fulfill
+    return Promise.resolve(promise);
   } else {
-    // Reject
+    return Promise.reject(promise);
   }
 }
